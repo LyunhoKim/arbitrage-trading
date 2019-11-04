e5daa3c5b349d4e07d6c3e81759a2942f185e12f
Lyunho Kim <lyunho.kim@doosan.com>
2019-11-04 14:58:17 +0900

    WIP app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const ccxt = require('ccxt');
 
-console.log(ccxt.exchanges);
+(async () => {
+    const upbit = new ccxt.upbit()
+    await upbit.loadMarkets()
+    const orderBook = await fetchLOrderBook('BTC/KRW');
+    console.log(upbit.id, orderBook);
+    // upbit.fetchL2OrderBook
+    
+}) ()
+
+// (async () => {
+//     const bithumb = new ccxt.bithumb()
+//     await bithumb.loadMarkets()
+//     console.log(bithumb.id, bithumb.market('BTC/KRW'));
+
+// }) ()
