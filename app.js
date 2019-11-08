cf19b28e504b3ecadff40080466aaeaee4decc3e
Lyunho Kim <lyunho.kim@doosan.com>
2019-11-08 09:02:47 +0900

    diff logic finished

--- app.js
+++ app.js
@@ -9,44 +9,35 @@ const pair = 'BTC/KRW';
 
 
 let exchanges = {};
+let markets = [];
 
 // asks 매도
 // bids 매수
 
+(async () => {  
 
-
-(async () => {
-  console.log("1111111111111111111111111111111111");
-  for (let exchange of exchangesConfig) {
-
-    console.log(exchange.id);
+  // 마켓 정보 조회
+  for (let exchange of exchangesConfig) {    
     exchanges[exchange.id] = new ccxt[exchange.id](exchange.info);
-
-    let markets = await exchanges[exchange.id].loadMarkets(pair);
-    // console.log('#####################################', markets[pair]);
-
-
-    // let orderBook = await exchanges[exchange.id].fetchOrderBook(pair);
-    // console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$', orderBook);       
-
+    markets[exchange.id] = (await exchanges[exchange.id].loadMarkets(pair))[pair];
+    // console.log('#####################################', markets);   
   }
+
+  // 각 거래소 별 오더북 조회
   async.parallel(
     [
       (callback) => {
         exchanges['upbit'].fetchOrderBook(pair).then(function (orderbook) {
-          console.log(orderbook);
           callback(null, orderbook);
         });
       },
       (callback) => {
         exchanges['bithumb'].fetchOrderBook(pair).then(function (orderbook) {
-          // console.log(orderbook);
           callback(null, orderbook);
         });
       },
       (callback) => {
         exchanges['coinone'].fetchOrderBook(pair).then(function (orderbook) {
-          // console.log(orderbook);
           callback(null, orderbook);
         });
       },
@@ -54,35 +45,76 @@ let exchanges = {};
     (error, result) => {
       console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
       // console.log('result: ', result);
-      console.log('error:', error);
+      if(error) {
+        console.log('error:', error);
+        return;
+      }     
+      
 
       let orders = [];
       orders['upbit'] = {
-        firstAskPrice: result[0].asks[0][0],
-        firstAskAmount: result[0].asks[0][1],
-        firstBidPrice: result[0].bids[0][0],
-        firstBidAmount: result[0].bids[0][1],
+        topAskPrice: result[0].asks[0][0],
+        topAskAmount: result[0].asks[0][1],
+        topBidPrice: result[0].bids[0][0],
+        topBidAmount: result[0].bids[0][1],
+        maker: markets['upbit'].maker,
+        taker: markets['upbit'].taker
       };
 
       orders['bithumb'] = {
-        firstAskPrice: result[1].asks[0][0],
-        firstAskAmount: result[1].asks[0][1],
-        firstBidPrice: result[1].bids[0][0],
-        firstBidAmount: result[1].bids[0][1],
+        topAskPrice: result[1].asks[0][0],
+        topAskAmount: result[1].asks[0][1],
+        topBidPrice: result[1].bids[0][0],
+        topBidAmount: result[1].bids[0][1],
+        maker: markets['bithumb'].maker,
+        taker: markets['bithumb'].taker
       };
 
       orders['coinone'] = {
-        firstAskPrice: result[2].asks[0][0],
-        firstAskAmount: result[2].asks[0][1],
-        firstBidPrice: result[2].bids[0][0],
-        firstBidAmount: result[2].bids[0][1],
+        topAskPrice: result[2].asks[0][0],
+        topAskAmount: result[2].asks[0][1],
+        topBidPrice: result[2].bids[0][0],
+        topBidAmount: result[2].bids[0][1],
+        maker: markets['coinone'].maker,
+        taker: markets['coinone'].taker
       };
-
-
       console.log(orders);
 
+      for(let i = 0; i<orders.length; i++) {
+        for(let j = i+1; j<orders.length; j++) {
+          let diff = orders[i].topAskPrice - orders[j].topBidPrice;
+          if(2000 < diff) {
+            let minAmount = Math.min(orders[i].topAskAmount, orders[j].topBidAmount);
+
+            // minAmount 만큼 수량으로 거래 요청
+            // requestTrade 1 i거래소에서 매수
+            // requestTrade 2 j거래소에서 매도
+
+            let askResult = orders[i].topAskPrice * minAmount * (1 - orders[i].taker);
+            let bidResult = orders[j].topBidPrice * minAmount * (1 - orders[j].taker);
+
+            console.log("##### Traded #####");
+            console.log('Result: ', askResult - bidResult);
+          }
+
+          diff = orders[j].topAskPrice - orders[i].topBidPrice;
+          if(2000 < diff) {
+            let minAmount = Math.min(orders[j].topAskAmount, orders[i].topBidAmount);
+
+            // minAmount 만큼 수량으로 거래 요청
+            // requestTrade 1 j거래소에서 매수
+            // requestTrade 2 i거래소에서 매도
+
+            let askResult = orders[j].topAskPrice * minAmount * (1 - orders[j].taker);
+            let bidResult = orders[i].topBidPrice * minAmount * (1 - orders[i].taker);
+
+            console.log("##### Traded #####");
+            console.log('Result: ', askResult - bidResult);
+          }
+        }
+      }
     }
-  );
+  )
 })();
 
 
