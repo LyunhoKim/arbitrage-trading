fa6dc9a32b204e8380df3a5700ccc5776a78f27d
Lyunho Kim <kimlh2@gmail.com>
2019-12-01 23:16:34 +0900

    upbit bot fix

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
 'use strict';
 
 const ccxt = require('ccxt');
+const exchangesConfig = require('./config/exchangesConfig.json');
 
 (async () => {
   const upbit = new ccxt['upbit'];
-  upbit.apiKey = 'JZbKMEuTsL0BcbbJeIcAabPu7QcA5CS0MT7lXdYK';
-  upbit.secret = 'KVgdCkF2OoeJpWKNYsdZn9poZKUxyLDoy3WFGxjT';
-  upbit.options['createMarketBuyOrderRequiresPrice'] = true;
+  upbit.apiKey = exchangesConfig.test.apiKey;
+  upbit.secret = exchangesConfig.test.secret;
+  upbit.options['createMarketBuyOrderRequiresPrice'] = false;
   // console.log(upbit.has);
   // console.log(await upbit.fetchBalance());
-  const order = await upbit.createOrder ('BTC/KRW', 'market', 'buy', 0.001, 9000000);
+  // const order = await upbit.createOrder ('BTC/KRW', 'market', 'buy', 0.0001, 8700000);
+  const order = await upbit.createOrder ('ETHBTC', 'market', 'buy', 0.01, 0.02011018);
+  // const order = await upbit.createMarketBuyOrder('BTC/KRW', 870);
+  // const order = await upbit.createMarketBuyOrder('ETH/BTC', 0.01 * 0.02011018);
+
   console.log(order);
 }) ();
--- upbitBot.js
+++ upbitBot.js
@@ -4,186 +4,136 @@ const ccxt = require('ccxt');
 const async = require('async');
 const fs = require('fs');
 
-
-const upbitInfo = {
-  id: "upbit",
+// ccxt object
+let upbit;
+let counter = 0;
+let bitcoin = 0;
+let upbitInfo = {
+  id: 'upbit',
   info: {
-      apiKey: "aaa",
-      secret: "bbb",
-      enableRateLimit: true
+    apiKey: "aaa",
+    secret: "bbb",
+    enableRateLimit: true
   }
 };
 
+// all symbols
+let symbols = [];
 
-// const pairs = require('pairs.json');
-const pair = 'BTC/KRW';
 
-
-let upbit;
-let KRWMarkets;
-let BTCMarkets;
+const bitTicker = () => {
+  upbit.fetchTicker('BTC/KRW').then((ticker) => {
+    bitcoin =  ticker ? ticker.last : 0;
+  });  
+  setTimeout(bitTicker, 2000);
+} 
 
 
 // asks 매도
 // bids 매수
+const main = async () => {
+
+  const idx = counter % symbols.length;
+  let order = symbols[idx];
+    async.parallel(
+      [
+        (callback) => {
+          upbit.fetchOrderBook(order.krw, 1).then(function (orderbook) {
+            callback(null, orderbook);
+          });
+        },
+        (callback) => {
+          upbit.fetchOrderBook(order.btc, 1).then(function (orderbook) {
+            callback(null, orderbook);
+          });
+        }  
+      ],
+      (error, result) => {
+        counter++;
+        
+        if(error) {
+          console.log('error:', error);
+          return;
+        }
+        order.bitcoinTicker = bitcoin;
+        order.krwOrder = {
+          topAskPrice: result[0].asks[0][0], topAskAmount: result[0].asks[0][1],
+          topBidPrice: result[0].bids[0][0], topBidAmount: result[0].bids[0][1]
+        }
+        order.btcOrder = {
+          topAskPrice: result[1].asks[0][0] * order.bitcoinTicker, // btcOrder는 btc 가격 표기가 맞으나 구현 편의상 원화로 표시
+          topAskPriceBtc: result[1].asks[0][0],                           // 원래의 btc 가격 표기
+          topAskAmount: result[1].asks[0][1],
+          topBidPrice: result[1].bids[0][0] * order.bitcoinTicker,
+          topBidPriceBtc: result[1].bids[0][0],
+          topBidAmount: result[1].bids[0][1]
+        }
+        
+        
+        console.log(order);
+        calcProfit(order.btcOrder, order.krwOrder, order);
+        calcProfit(order.krwOrder, order.btcOrder, order);
+        
+        setTimeout(main, 1000);
+      }
+    )
+};
 
 
 
-// const main = async () => {
-//   // 각 거래소 별 오더북 조회
-//   async.parallel(
-//     [
-//       (callback) => {
-//         exchanges['upbit'].fetchOrderBook(pair).then(function (orderbook) {
-//           callback(null, orderbook);
-//         });
-//       },      
-//     ],
-//     (error, result) => {
-//       console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
-//       // console.log('result: ', result);
-//       if(error) {
-//         console.log('error:', error);
-//         return;
-//       }     
-      
-
-//       let orders = {
-//         upbit: {
-//           topAskPrice: result[0].asks[0][0],
-//           topAskAmount: result[0].asks[0][1],
-//           topBidPrice: result[0].bids[0][0],
-//           topBidAmount: result[0].bids[0][1],
-//           maker: markets['upbit'].maker,
-//           taker: markets['upbit'].taker
-//         },
-
-//         bithumb: {
-//           topAskPrice: result[1].asks[0][0],
-//           topAskAmount: result[1].asks[0][1],
-//           topBidPrice: result[1].bids[0][0],
-//           topBidAmount: result[1].bids[0][1],
-//           maker: markets['bithumb'].maker,
-//           taker: markets['bithumb'].taker
-//         },
-
-//         coinone: {
-//           topAskPrice: result[2].asks[0][0],
-//           topAskAmount: result[2].asks[0][1],
-//           topBidPrice: result[2].bids[0][0],
-//           topBidAmount: result[2].bids[0][1],
-//           maker: markets['coinone'].maker,
-//           taker: markets['coinone'].taker
-//         }
-//     };
-
-//     let ordersArray = [];
-//     ordersArray.push({
-//         symbol: 'upbit',
-//         topAskPrice: result[0].asks[0][0],
-//         topAskAmount: result[0].asks[0][1],
-//         topBidPrice: result[0].bids[0][0],
-//         topBidAmount: result[0].bids[0][1],
-//         maker: markets['upbit'].maker,
-//         taker: markets['upbit'].taker
-//       });
-
-//       ordersArray.push({
-//         symbol: 'bithumb',
-//         topAskPrice: result[1].asks[0][0],
-//         topAskAmount: result[1].asks[0][1],
-//         topBidPrice: result[1].bids[0][0],
-//         topBidAmount: result[1].bids[0][1],
-//         maker: markets['bithumb'].maker,
-//         taker: markets['bithumb'].taker
-//       });
-
-//       ordersArray.push({
-//         symbol: 'coinone',
-//         topAskPrice: result[2].asks[0][0],
-//         topAskAmount: result[2].asks[0][1],
-//         topBidPrice: result[2].bids[0][0],
-//         topBidAmount: result[2].bids[0][1],
-//         maker: markets['coinone'].maker,
-//         taker: markets['coinone'].taker
-//       });
-    
-      
-      
-      
-//       let tradeTarget = [];
-//       for(let i = 0; i<ordersArray.length; i++) {
-//         for(let j = i+1; j<ordersArray.length; j++) {
-//           let diff = ordersArray[j].topBidPrice - ordersArray[i].topAskPrice;
-//           console.log(`diff: ${diff}, ${ordersArray[j].symbol} - ${ordersArray[i].symbol}`);
-//           if(2000 <= diff) {
-//             let minAmount = Math.min(ordersArray[i].topAskAmount, ordersArray[j].topBidAmount);
-
-//             // minAmount 만큼 수량으로 거래 요청
-//             // requestTrade 1 i거래소에서 매수
-//             // requestTrade 2 j거래소에서 매도
-
-//             let bidResult = ordersArray[j].topBidPrice * minAmount * (1 - ordersArray[j].taker); // 수수료반영된 매도 원화금액
-//             let askedFee = ordersArray[i].topAskPrice * minAmount * ordersArray[i].taker; // 매수한 후 수수료
-
-//             //예상 수익                    
-//             tradeTarget.push(`${getTimeStamp()}, 예상수익:${(bidResult - askedFee).toFixed(3)},\t 시세차:${diff}, ${ordersArray[j].symbol}매수가:${ordersArray[j].topBidPrice}, ${ordersArray[i].symbol}매도가:${ordersArray[i].topAskPrice}, 거래량:${minAmount}<br>`);
-//             console.log("##### Traded #####");
-//             console.log('Result: ', bidResult - askedFee);
-//           }
-
-//           diff = ordersArray[i].topBidPrice - ordersArray[j].topAskPrice;
-//           console.log(`diff: ${diff}, ${ordersArray[i].symbol} - ${ordersArray[j].symbol}`);
-//           if(2000 <= diff) {
-//             let minAmount = Math.min(ordersArray[j].topAskAmount, ordersArray[i].topBidAmount);
-
-//             // minAmount 만큼 수량으로 거래 요청
-//             // requestTrade 1 j거래소에서 매수
-//             // requestTrade 2 i거래소에서 매도
-
-//             let askFee = ordersArray[j].topAskPrice * minAmount * ordersArray[j].taker;
-//             let bidResult = ordersArray[i].topBidPrice * minAmount * (1 - ordersArray[i].taker);
-
-//             tradeTarget.push(`${getTimeStamp()}, 예상수익:${(bidResult - askFee).toFixed(3)},\t 시세차:${diff}, ${ordersArray[i].symbol}매수가:${ordersArray[i].topBidPrice}, ${ordersArray[j].symbol}매도가:${ordersArray[j].topAskPrice}, 거래량:${minAmount}<br>`)
-//             console.log("##### Traded #####");
-//             console.log('Result: ', bidResult - askFee);
-            
-//           }
-//         }
-//       }
-//       // res.json('aaaaaaa');
-//       console.log("##### response #####");      
-//       console.log(orders);
-//       console.log(tradeTarget);
-      
-//       for(let i=0; i<tradeTarget.length; i++) {
-//         fs.appendFile('trigger-log.log', tradeTarget[i], 'utf8', (error, data) => {
-//           if(error)
-//             console.log(`file writing error`);
-//           else
-//           console.log(`file writing success`);
-//         });
-//       }
-      
-//     }
-//   )
-
-//   setTimeout(main, 10000);
-// };
+function calcProfit(bids, asks, order) {
+  
+  let diff = bids.topBidPrice - asks.topAskPrice;
+  let debug = `[${order.base}]bids: ${bids.topBidPrice}, asks: ${asks.topAskPrice}, diff: ${diff}`;
+  consoleDebug(debug);
+  if(0 < diff) {
+    let minAmount = Math.min(bids.topBidAmount, asks.topAskAmount) * 0.5;
+    let profit = diff * minAmount - bids.topBidPrice * minAmount * order.taker
+                                  - asks.topAskPrice * minAmount * order.taker;
+    // consoleDebug(`minAmount: ${minAmount}, profit: ${profit}`);
+    if(5.0 <= profit ) {      
+      let result = `#${leadingZeros(counter, 8)} [${getTimeStamp()}] KRW->BTC ${order.base}: ${profit.toFixed(1)}원 수량: ${minAmount}\n<br>`;             
+      fs.appendFile('trigger-log.log', result, 'utf8', (error, data) => {});
+      // consoleDebug(result);
+      console.log(result);
+      return profit;
+    }
+  } 
+  return 0; 
+}
 
 (async function getMarketInfo() {
+
+  consoleDebug('bot started');
+  let markets = [];
   // 마켓 정보 조회
-
-  upbit = new ccxt[upbitInfo.id](upbitInfo.info);
-  let markets = await upbit.loadMarkets();
+  upbit = new ccxt[upbitInfo.id]();
+  markets = await upbit.loadMarkets();  
   markets = Object.values(markets);
-  KRWMarkets = markets.filter(pair => pair.symbol.includes("/KRW"));
-  BTCMarkets = markets.filter(pair => pair.symbol.includes("/BTC"));
 
-  console.log('#####################################', KRWMarkets); 
-  console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$', BTCMarkets); 
+  // krw, btc 마켓
+  let krwMarket = markets.filter(pair => pair.symbol.includes('/KRW'));
+  let btcMarket = markets.filter(pair => pair.symbol.includes('/BTC'));
 
-  // setTimeout(main, 5000);
+  // krw, btc 공통 마켓만 분리
+  for(let i=0; i<krwMarket.length; i++) {
+    for(let j=0; j<btcMarket.length; j++) {
+      if((krwMarket[i].base) === (btcMarket[j].base)) {
+        let symbol = {
+          base: krwMarket[i].base,
+          krw: krwMarket[i].symbol,
+          btc: btcMarket[j].symbol,
+          krwTaker: krwMarket[i].taker,
+          btcTaker: btcMarket[i].taker          
+        }
+        symbol.btcOrder = { taker: btcMarket[i].taker };
+        symbol.krwOrder = { taker: krwMarket[i].taker };
+        symbols.push(symbol);
+      }
+    }
+  }; 
+  setTimeout(bitTicker, 0);  
+  setTimeout(main, 3000);
 }) ();
 
 function getTimeStamp() {
@@ -209,6 +159,10 @@ function leadingZeros(n, digits) {
       zero += '0';
   }
   return zero + n;
+}
+
+function consoleDebug(msg) {
+  console.log('\x1b[36m', msg);
 }
 
 
